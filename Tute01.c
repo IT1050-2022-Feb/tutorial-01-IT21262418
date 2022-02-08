c9f40e2225da75b1b3238213e6b3d376c6557fe2
IT21262418 <99236610+IT21262418@users.noreply.github.com>
2022-02-08 05:43:53 +0000

    complete 1st tutorial

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+
+  float mark1;
+  float mark2;
+
+  printf("Enter mark 1 :");
+  scanf("%f",&mark1);
+  printf("Enter mark 2 :");
+  scanf("%f",&mark2);
+
+  printf("\n\nthe average of the two marks is :%.2f",(mark1 + mark2)/2);
   
   return 0;
 }
